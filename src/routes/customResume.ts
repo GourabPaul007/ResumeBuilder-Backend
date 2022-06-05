8a7692a1b8ca954ab85b13e0e129087d7edef398
Gourab Paul <gourabpaul900@gmail.com>
2022-06-05 20:35:45 +0530

    moved skillsDiv out of ejs file for better modularity & control

--- src/routes/customResume.ts
+++ src/routes/customResume.ts
@@ -27,6 +27,39 @@ const makePdf = async (details: any) => {
     projectsDiv: details[1],
     skillsDiv: details[2],
     educationsDiv: details[3],
+    skills2: `
+    <div
+      style="
+        position: absolute;
+        margin-bottom: 8px;
+        padding: 8px;
+        background-color: white;
+        left: ${details[2].x * 21.0}mm;
+        top: ${details[2].y * 40}px;
+        width: ${details[2].w * 21.0}mm;
+        height: ${details[2].h * 40}px;
+      "
+    >
+      <h1 style="font-weight: 600; color: #123456">Skills</h1>
+      <div style="margin-top: 8px; padding-left: 8px">
+        ${details[2].data
+          .map((eachSkill: string) => {
+            return `<div
+            style="
+              display: inline-block;
+              padding: 4px 8px;
+              margin: 4px;
+              border-radius: 6px;
+              color: #fff;
+              background-color: #123456;
+            "
+          >
+          ${eachSkill}
+        </div>`;
+          })
+          .join("")}
+      </div>
+    </div>`,
   });
   // Puppeteer
   const browser = await puppeteer.launch({
