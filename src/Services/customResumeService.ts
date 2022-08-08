72c92eb08c2d1c94b6a32dc8dadff83cffe591df
Gourab Paul <gourabpaul900@gmail.com>
2022-08-08 15:31:17 +0530

    restructure - wrapped methods in classes

--- src/Services/customResumeService.ts
+++ src/Services/customResumeService.ts
@@ -12,82 +12,121 @@ import { FormStyles } from "../interfaces/FormStyles";
 import { AboutSection1 } from "../resumeTemplates/custom/AboutSections";
 import { ContactSection1, ContactSection2 } from "../resumeTemplates/custom/ContactSections";
 import { RatingsSection1 } from "../resumeTemplates/custom/RatingsSections";
+import { DocumentData, getFirestore } from "firebase-admin/firestore";
+import { firebaseApp } from "..";
 
-const organizeData = (element: any, formStyles: FormStyles) => {
-  // details.forEach(
-  //   (element: {
-  //     name?: any;
-  //     x: number;
-  //     y: number;
-  //     w: number;
-  //     h: number;
-  //     data: About | Project[] | Work[] | Course[] | string[]|any;
-  //   }) => {
-  console.log("element", element.data);
+export class CustomResumeService {
+  /**
+   * takes resume data from firebase with provided resumeID and returns the resume data
+   * @param resumeID the ID which identfies resume in firebase firestore
+   * @returns resume data from firebase to makePdf
+   */
+  getPdfDataFromFirebase = async (resumeID: string) => {
+    let blocks: any[] = [];
+    let formStyles: FormStyles = {
+      titleFullWidth: false,
+      titleFilled: false,
+      titleUnderline: false,
+      titleColor: "",
+      titleFillColor: "",
+      accentColor: "",
+    };
 
-  switch (element.name) {
-    case "aboutwithcontact1":
-      return AboutWithContactSection1(element, formStyles);
-    case "aboutwithcontact2":
-      return AboutWithContactSection2(element, formStyles);
-    case "about1":
-      return AboutSection1(element, formStyles);
-    case "contact1":
-      return ContactSection1(element, formStyles);
-    case "contact2":
-      return ContactSection2(element, formStyles);
-    case "educations1":
-      return EducationsSection1(element, formStyles);
-    case "educations2":
-      return EducationsSection2(element, formStyles);
-    case "skills1":
-      return SkillsSection1(element, formStyles);
-    case "skills2":
-      return SkillsSection2(element, formStyles);
-    case "works1":
-      return WorksSection1(element, formStyles);
-    case "projects1":
-      return ProjectsSection1(element, formStyles);
-    case "ratings1":
-      return RatingsSection1(element, formStyles);
-    case "others1":
-      return OthersSection1(element, formStyles);
-    default:
-      break;
-  }
-  //   }
-  // );
-};
+    if (resumeID == "" || resumeID == null || resumeID == undefined)
+      return { success: false, errMsg: "resumeID not found.", data: { blocks, formStyles } };
 
-export const getPdfDataFromFirebase = async () => {
-  // return {blocks,formStyles};
-};
+    const defaultFirestore = getFirestore(firebaseApp);
 
-export const makePdf = async (blocks: any[], formStyles: FormStyles) => {
-  console.log(formStyles.titleFillColor);
+    const documentRef = defaultFirestore.collection("resumes").doc(resumeID);
+    await documentRef.get().then((doc: DocumentData) => {
+      blocks = doc.data().resumeData.blocks;
+      formStyles = doc.data().resumeData.formStyles;
+    });
+    return { success: true, errMsg: "", data: { blocks, formStyles } };
+  };
 
-  const templateString = await ejs.renderFile(path.join(__dirname, "..", "resumeTemplates", `custom.ejs`), {
-    isDark: false,
-    checkHyperlink: checkHyperlink,
-    blocks: blocks.map((blockDetail: any) => {
-      return organizeData(blockDetail, formStyles);
-    }),
-    formStyles: formStyles,
-  });
-  // Puppeteer
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.setContent(templateString, {
-    waitUntil: "networkidle2",
-  });
-  const pdf = await page.pdf({
-    format: "a4",
-    printBackground: true,
-    margin: { left: 0, right: 0, top: 24, bottom: 24 },
-  });
-  await browser.close();
-  return pdf;
-};
+  /**
+   * checks if the document is good/correct
+   * @param formStyles the global formStyles
+   * @returns true or false
+   */
+  checkResumeDocument = (formStyles: FormStyles) => {
+    if (formStyles.accentColor != "" && formStyles.titleColor != "" && formStyles.titleFillColor != "") return true;
+    return false;
+  };
+
+  /**
+   * Gets block name and returns section according to the name
+   * @param element single block of resume
+   * @param formStyles global styles of resume
+   * @returns single section of resume
+   */
+  organizeData = (element: any, formStyles: FormStyles) => {
+    // console.log("element", element.data);
+    switch (element.name) {
+      case "aboutwithcontact1":
+        return AboutWithContactSection1(element, formStyles);
+      case "aboutwithcontact2":
+        return AboutWithContactSection2(element, formStyles);
+      case "about1":
+        return AboutSection1(element, formStyles);
+      case "contact1":
+        return ContactSection1(element, formStyles);
+      case "contact2":
+        return ContactSection2(element, formStyles);
+      case "educations1":
+        return EducationsSection1(element, formStyles);
+      case "educations2":
+        return EducationsSection2(element, formStyles);
+      case "skills1":
+        return SkillsSection1(element, formStyles);
+      case "skills2":
+        return SkillsSection2(element, formStyles);
+      case "works1":
+        return WorksSection1(element, formStyles);
+      case "projects1":
+        return ProjectsSection1(element, formStyles);
+      case "ratings1":
+        return RatingsSection1(element, formStyles);
+      case "others1":
+        return OthersSection1(element, formStyles);
+      default:
+        break;
+    }
+  };
+
+  /**
+   * makes the actual pdf from blocks and styles
+   * @param blocks the resume blocks
+   * @param formStyles the resume global styles
+   * @returns pdf document
+   */
+  makePdf = async (blocks: any[], formStyles: FormStyles) => {
+    console.log(formStyles.titleFillColor);
+
+    const templateString = await ejs.renderFile(path.join(__dirname, "..", "resumeTemplates", `custom.ejs`), {
+      isDark: false,
+      checkHyperlink: checkHyperlink,
+      blocks: blocks.map((blockDetail: any) => {
+        return this.organizeData(blockDetail, formStyles);
+      }),
+      formStyles: formStyles,
+    });
+    // Puppeteer
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.setContent(templateString, {
+      waitUntil: "load",
+    });
+    const pdf = await page.pdf({
+      format: "a4",
+      printBackground: true,
+      margin: { left: 0, right: 0, top: 24, bottom: 24 },
+    });
+    await browser.close();
+    return pdf;
+  };
+}
--- src/routes/customResumeRoute.ts
+++ src/routes/customResumeRoute.ts
@@ -1,33 +1,35 @@
 import express from "express";
-import { DocumentData, getFirestore } from "firebase-admin/firestore";
-import { firebaseApp } from "..";
-import { makePdf } from "../Services/customResumeService";
+import { CustomResumeService } from "../Services/customResumeService";
 
 const router = express.Router();
 
-router.get("/get-pdf/:id", async (req, res) => {
+router.get("/get-pdf/:resumeID", async (req, res) => {
   const params = req.params;
-  const defaultFirestore = getFirestore(firebaseApp);
-  if (params.id != "" || params.id != null || params.id != undefined) {
-    defaultFirestore
-      .collection("resumes")
-      .doc(params.id)
-      .get()
-      .then(async (doc: DocumentData) => {
-        console.log("RESUME DATA: ", doc.data().resumeData);
-        const pdf = await makePdf(doc.data().resumeData.blocks, doc.data().resumeData.formStyles);
-        res.header("Content-type", "application/pdf");
-        res.send(pdf);
-      });
-  } else {
-    console.log("ID not found!");
+
+  const customResumeService = new CustomResumeService();
+  const { success, errMsg, data } = await customResumeService.getPdfDataFromFirebase(params.resumeID);
+  // In case error occurs and success is false(data is not found), it will send the error message and exit the function
+  if (success == false) {
+    res.status(400).send({ error: errMsg });
+    return;
   }
+  // If no error, continue with data
+  const { blocks, formStyles } = data;
+  // If resume document have empty data, send error msg to cliend and exit the function
+  if (customResumeService.checkResumeDocument(formStyles) == false) {
+    res.status(400).send({ error: "resume document doesnt have enough data(formstyles)" });
+    return;
+  }
+
+  const pdf = await customResumeService.makePdf(blocks, formStyles);
+  res.header("Content-type", "application/pdf");
+  res.send(pdf);
 });
 
 router.post("/custom-resume", express.json(), async (req, res) => {
   const { blocks, formStyles } = req.body;
   console.log("details received", blocks);
-  const pdf = await makePdf(blocks, formStyles);
+  const pdf = await new CustomResumeService().makePdf(blocks, formStyles);
   // resumeTemplateNo = details.template;
   // new ResumeService(resumeTemplateNo).setResumeData(details, details.resumeId);
   res.header("Content-type", "application/pdf");
